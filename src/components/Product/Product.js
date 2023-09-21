8349ed9ffec7b37d23fa625be64554cb554f2a4c
Robert Synowiec <rsynowiec81@gmail.com>
2023-09-21 21:28:45 +0200

    Add function that changes the color of the T-shirt

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -1,24 +1,34 @@
 import styles from './Product.module.scss';
 import clsx from 'clsx';
 import Button from '../Button/Button';
+import PropTypes from 'prop-types';
+import { useState, useEffect } from 'react';
 
-const Product = props => {
+const Product = ({ colors, sizes, name, title, basePrice }) => {
+
+  const [currentColor, setCurrentColor] = useState(colors[0]);
+  const [currentSize, setCurrentSize] = useState(sizes[0].name);
+
+  const prepareColorClassName = color => {
+    return styles['color' + color[0].toUpperCase() + color.substr(1).toLowerCase()];
+  }
+
   return (
     <article className={styles.product}>
       <div className={styles.imageContainer}>
-        <img 
+        <img
           className={styles.image}
-          alt="Kodilla shirt"
-          src={`${process.env.PUBLIC_URL}/images/products/shirt-kodilla--black.jpg`} />
+          alt={title}
+          src={`${process.env.PUBLIC_URL}/images/products/shirt-${name}--${currentColor}.jpg`} />
       </div>
       <div>
         <header>
-          <h2 className={styles.name}>Kodilla shirt</h2>
-          <span className={styles.price}>Price: 20$</span>
+          <h2 className={name}>{title}</h2>
+          <span className={styles.price}>Price: {basePrice}$</span>
         </header>
         <form>
           <div className={styles.sizes}>
-            <h3 className={styles.optionLabel}>Sizes</h3>
+            <h3 className={styles.optionLabel}>{currentSize}</h3>
             <ul className={styles.choices}>
               <li><button type="button" className={styles.active}>S</button></li>
               <li><button type="button">M</button></li>
@@ -29,10 +39,15 @@ const Product = props => {
           <div className={styles.colors}>
             <h3 className={styles.optionLabel}>Colors</h3>
             <ul className={styles.choices}>
-              <li><button type="button" className={clsx(styles.colorBlack, styles.active)} /></li>
-              <li><button type="button" className={clsx(styles.colorRed)} /></li>
-              <li><button type="button" className={clsx(styles.colorWhite)} /></li>
+              {colors.map(color =>
+                <li key={color}>
+                  <button type="button"
+                    className={clsx(prepareColorClassName(color), color === currentColor && styles.active)} />
+                </li>
+              )}
+
             </ul>
+
           </div>
           <Button className={styles.button}>
             <span className="fa fa-shopping-cart" />
@@ -43,4 +58,5 @@ const Product = props => {
   )
 };
 
+Product.propTypes = { props: PropTypes.array };
 export default Product;

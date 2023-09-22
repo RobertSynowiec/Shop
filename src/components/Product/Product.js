379789b92306d73e3de916716f5f582d3aaed4c3
Robert Synowiec <rsynowiec81@gmail.com>
2023-09-22 19:16:50 +0200

    Add the ability to add up the basic price and additional price

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -2,13 +2,33 @@ import styles from './Product.module.scss';
 import clsx from 'clsx';
 import Button from '../Button/Button';
 import PropTypes from 'prop-types';
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
 import shortid from 'shortid';
 
 const Product = ({ colors, sizes, name, title, basePrice }) => {
 
   const [currentColor, setCurrentColor] = useState(colors[0]);
   const [currentSize, setCurrentSize] = useState(sizes[0].name);
+  const [currentSizeAdditionalPrice, setCurrentSizeAdditionalPrice] = useState(sizes[0].additionalPrice);
+
+  // Handle button click
+  const handleButtonClick = (index) => {
+
+    // Check if index is in the index range of the products array
+    if (index >= 0 && index < sizes.length) {
+
+      // Get the size name and additionalPrice based on the index
+      const { name, additionalPrice } = sizes[index];
+
+      // Update states
+      setCurrentSize(name);
+      setCurrentSizeAdditionalPrice(additionalPrice);
+    }
+  };
+
+  const getPrice = () => {
+    return basePrice += currentSizeAdditionalPrice
+  }
 
   const prepareColorClassName = color => {
     return styles['color' + color[0].toUpperCase() + color.substr(1).toLowerCase()];
@@ -25,17 +45,19 @@ const Product = ({ colors, sizes, name, title, basePrice }) => {
       <div>
         <header>
           <h2 className={name}>{title}</h2>
-          <span className={styles.price}>Price: {basePrice}$</span>
+          <span className={styles.price}>Price: {getPrice()}$</span>
         </header>
         <form>
           <div className={styles.sizes}>
             <h3 className={styles.optionLabel}>{currentSize}</h3>
             <ul className={styles.choices}>
 
-              {sizes.map(size =>
-                <li key={shortid.generate()} value={size}>
+              {sizes.map((size, index) =>
+                <li key={index} value={size}>
                   <button type="button"
-                    className={clsx(size.name === currentSize && styles.active)} value={size.name} onClick={e => setCurrentSize(e.target.value)} >
+                    className={clsx(size.name === currentSize && styles.active)} value={size.name}
+                    //clicking the button, we pass the product index to the handleButtonClick function
+                    onClick={() => handleButtonClick(index)} >
                     {size.name}
                   </button>
                 </li>
